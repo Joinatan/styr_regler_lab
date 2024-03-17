cb1bfe3a9b6f0b4cc702d149311cc43a49312106
Joinatan <eklund.jonatan@gmail.com>
2024-03-17 21:08:58 +0100

    uppgift 5 typ klar

--- simulering5.m
+++ simulering5.m
@@ -1,9 +1,13 @@
 %simuleringsprogram polplaceringsregulator
 N=15;
 h = 1;
+%utsignal
 y_k = step(Gz_tot, N);
+%styrsignal
+u_k = step(Uz, N);
 
 for k=0:N
-   disp(y_k(k+1))
+   disp(k)
+   disp(u_k(k+1))
 end
 
